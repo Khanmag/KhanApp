35082ecf903bb51a3e00d4e5c3fd352cd24dc180
Khanmag <Khanmag329@gmail.com>
2023-02-12 17:41:44 +0300

    update media css design

--- src/data/creativityData.ts
+++ src/data/creativityData.ts
@@ -1,28 +1,43 @@
-import animeIMG from "../images/forCreativityPage/anime.png";
-import moviesIMG from "../images/forCreativityPage/movies.jpeg";
-import musicIMG from "../images/forCreativityPage/music.jpeg";
-import booksIMG from "../images/forCreativityPage/books.jpeg";
-import paintingsIMG from "../images/forCreativityPage/paintings.jpeg";
+import anime from "../images/forCreativityPage/anime.png";
+import comics from "../images/forCreativityPage/comics.jpeg";
+import serials from "../images/forCreativityPage/serials.jpeg";
+import manga from "../images/forCreativityPage/manga.jpeg";
+import movies from "../images/forCreativityPage/movies.jpeg";
+import music from "../images/forCreativityPage/music.jpeg";
+import books from "../images/forCreativityPage/books.jpeg";
+import art from "../images/forCreativityPage/art.jpeg";
 
 export const creativityData = [
     {
         name: 'anime',
-        image: animeIMG,
+        image: anime,
+    },
+    {
+        name: 'comics',
+        image: comics,
+    },
+    {
+        name: 'serials',
+        image: serials,
+    },
+    {
+        name: 'manga',
+        image: manga,
     },
     {
         name: 'movies',
-        image: moviesIMG,
-    },
-    {
-        name: 'music',
-        image: musicIMG,
-    },
-    {
-        name: 'books',
-        image: booksIMG,
+        image: movies,
     },
     {
         name: 'art',
-        image: paintingsIMG,
+        image: art,
+    },
+    {
+        name: 'music',
+        image: music,
+    },
+    {
+        name: 'books',
+        image: books,
     },
 ]
